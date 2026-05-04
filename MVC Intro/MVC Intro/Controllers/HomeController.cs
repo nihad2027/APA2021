d00ca35c6f19e958e66e9306b2073e57578e647e
Nihad2027 <nihadea-apa202@code.edu.az>
2026-05-04 21:20:23 +0400

    update commit

---
+++ MVC Intro/MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("APA202");
+
+            //var student = new JsonResult(new { id = 1, name = "Ali", surname = "Quliyev" });
+
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return RedirectToAction(nameof(Index),"Product");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC Intro/MVC Intro/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Intro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
